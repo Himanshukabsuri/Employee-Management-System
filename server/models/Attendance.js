3f47083705c721b48aa69beee4950e68dea8865b
Himanshukabsuri <hkabsuri@gmail.com>
2026-04-26 10:21:41 +0530

    api created for leave application

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -4,7 +4,7 @@ const attendanceSchema = new mongoose.Schema({
     employeeId:{type:mongoose.Schema.Types.ObjectId,ref:"Employee",required:true},
     date:{type:Date,required:true},
     checkIn:{type:Date,required:true},
-    checkOut:{type:Date,required:true},
+    checkOut: { type: Date, default: null },
     status:{type:String,enum:["PRESENT","ABSENT","LATE"],default:"PRESENT"},
     workingHours:{type:Number,default:null},
     dayType:{type:String,enum:["Full Day","Three Quarter Day","Half Day","Short Day",null],default:null}
@@ -14,6 +14,6 @@ const attendanceSchema = new mongoose.Schema({
 
 attendanceSchema.index({employeeId:1,date:1},{unique:true})
 
-const Attendance = mongoose.model("User",attendanceSchema)
+const Attendance = mongoose.model("Attendance",attendanceSchema)
 
 export default Attendance
