8cd916e5051e8159aebca2d26f9bd263413b31fd
Himanshukabsuri <hkabsuri@gmail.com>
2026-04-26 10:54:48 +0530

    api created for payslip

--- server/models/leaveApplications.js
+++ server/models/leaveApplications.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const leaveApplicationSchema = new mongoose.Schema({
     employeeId:{type:mongoose.Schema.Types.ObjectId,ref:"Employee",required:true},
     type:{type:String,enum:["SICK","CASUAL","ANNUAL"],required:true},
-    startDate: {tye:Date,required:true},
+    startDate: {type:Date,required:true},
     endDate:{type:Date,required:true},
     reason:{type:Date,required:true},
     status:{type:Date,enum:["PENDING","APPROVED","REJECTED"],default:"PENDING"},
